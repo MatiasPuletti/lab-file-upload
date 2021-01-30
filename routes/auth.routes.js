3bd6283316e47ee0e9f0e147fb8c928b60b35139
Matias Puletti <matiaspuletti@Matiass-MacBook-Pro.local>
2021-01-30 10:08:07 +0100

    Iterations 1-2 done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,6 +8,8 @@ const User = require('../models/User.model');
 const mongoose = require('mongoose');
 
 const routeGuard = require('../configs/route-guard.config');
+const upload = require('../configs/cloudinary');
+
 
 ////////////////////////////////////////////////////////////////////////
 ///////////////////////////// SIGNUP //////////////////////////////////
@@ -17,7 +19,7 @@ const routeGuard = require('../configs/route-guard.config');
 router.get('/signup', (req, res) => res.render('auth/signup'));
 
 // .post() route ==> to process form data
-router.post('/signup', (req, res, next) => {
+router.post('/signup', upload.single("image"),(req, res, next) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
@@ -42,10 +44,8 @@ router.post('/signup', (req, res, next) => {
         // username: username
         username,
         email,
-        // passwordHash => this is the key from the User model
-        //     ^
-        //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
-        passwordHash: hashedPassword
+        passwordHash: hashedPassword,
+        imageUrl: req.file.path
       });
     })
     .then(userFromDB => {
